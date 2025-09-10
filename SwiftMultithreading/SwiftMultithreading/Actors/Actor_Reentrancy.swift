a1572059fb0c32ddda13f0efd033943846e0f9db
Raymond Truong <ray@raytruong.dev>
2025-09-10 08:45:37 -0700

    add actor reentrancy demo

---
+++ SwiftMultithreading/SwiftMultithreading/Actors/Actor_Reentrancy.swift
@@ -0,0 +1,39 @@
+import Playgrounds
+
+// This example shows how actor reentrancy can lead to a bug
+// Task 1 and Task 2 are not guaranteed execution ordering
+
+actor UserActor {
+    var name: String?
+
+    func setName(name: String) async {
+        // Simulate a network call
+        try? await Task.sleep(nanoseconds: 1_000_000_000)
+        self.name = name
+        print("Task 1: Name set to \(name)")
+    }
+
+    func printName() {
+        if let name = self.name {
+            print("Task 2: User name is \(name)")
+        } else {
+            print("Task 2: Name not yet available.")
+        
+    }
+}
+
+
+#Playground {
+    let userActor = UserActor()
+
+    // These two tasks run independently and the order of their completion is not guaranteed.
+    Task { await userActorUnreliable.setName(name: "Alice") }
+    Task { await userActorUnreliable.printName() }
+
+    // This task runs each step in structured order. This is similar to a critical section.
+    Task {
+        await userActor.setName(name: "Alice")
+        await userActor.printName()
+    }
+}
+
